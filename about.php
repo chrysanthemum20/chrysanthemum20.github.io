f0379080cfca2f4112dac03364afdfdd4b5ad8d2
chrysanthemum20 <150367965+chrysanthemum20@users.noreply.github.com>
2025-01-12 18:31:12 +0800

    creation of the portfolio

---
+++ about.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URIEL PACIS - Portfolio</title>
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <!-- NAVBAR  -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">QA Portfolio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">        
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="#skills">Skills</a></li> -->
+                    <li class="nav-item"><a class="nav-link" href="project.php">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>                 
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- About -->
+        <section id="about" class="py-5">
+            <div class="container">
+                <h2>About me</h2>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="images/profile.jpg" alt="" class="img-fluid rounded-circle">
+                    </div>
+                    <div class="col-md-8">
+                        <p>Hi! I'm an IT Graduate who is passionate for Quality Assurance and Software Engineering. 
+                    </div>
+                
+            </div>
+        </section>
+
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URIEL PACIS - Portfolio</title>
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <!-- NAVBAR  -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">QA Portfolio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">        
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="#skills">Skills</a></li> -->
+                    <li class="nav-item"><a class="nav-link" href="project.php">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>                 
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- HERO -->
+    <header class="hero bg-primary text-white text-center py-5">
+        <div class="container">
+            <h1>Hi, I am <span class="highlight">Uriel Pacis</span></h1>
+            <p class="lead">I am a Software Engineer with passion for Quality Assurance</p>
+        </div>
+    </header>
+
+    
+</body>
+</html>
